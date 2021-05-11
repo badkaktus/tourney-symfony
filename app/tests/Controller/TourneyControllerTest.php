532f351a62849d75c3c2151bc9b050b6186d72ff
badkaktus <T7J-Mhu-QRH-354>
2021-05-11 14:50:26 +0300

    final tests

--- app/tests/Controller/TourneyControllerTest.php
+++ app/tests/Controller/TourneyControllerTest.php
@@ -12,36 +12,56 @@ use Symfony\Component\DomCrawler\Crawler;
 class TourneyControllerTest extends WebTestCase
 {
     private AbstractBrowser $client;
-//    public function testResolveIndex()
-//    {
-//        $client = static::createClient();
-//        $client->request('GET', '/');
-//
-//        self::assertResponseIsSuccessful();
-//        self::assertSelectorTextContains('h2', 'Список турниров');
-//        self::assertSelectorTextContains('button', 'Создать турнир');
-//    }
 
-//    public function testCreateTourney(): void
-//    {
-//        $this->createRandomTourney();
-//        self::assertResponseStatusCodeSame(302);
-//    }
+    /**
+     * Тест главной страницы
+     */
+    public function testResolveIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
 
-//    public function testGetSingleTourney(): void
-//    {
-//        $this->createRandomTourney();
-//        self::assertMatchesRegularExpression(
-//            '/\/tourney\/\d+/',
-//            $this->client->getResponse()->headers->get('location'),
-//            'Неправильный URL турнира'
-//        );
-//        $crawler = $this->client->request('GET', $this->client->getResponse()->headers->get('location'));
-//        self::assertResponseIsSuccessful();
-//        self::assertCount(2, $crawler->filter('h3'));
-//        self::assertCount(2, $crawler->filter('button'));
-//    }
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h2', 'Список турниров');
+        self::assertSelectorTextContains('button', 'Создать турнир');
+    }
 
+    /**
+     * Тест создания турнира, проверяется редирект и URL, возвращаемый после создания
+     */
+    public function testCreateTourney(): void
+    {
+        $this->createRandomTourney();
+        self::assertResponseStatusCodeSame(302);
+        self::assertMatchesRegularExpression(
+            '/\/tourney\/\d+/',
+            $this->client->getResponse()->headers->get('location'),
+            'Неправильный URL турнира'
+        );
+    }
+
+    /**
+     * Тест получения данных для отображения конкретного турнира
+     */
+    public function testGetSingleTourney(): void
+    {
+        $this->createRandomTourney();
+        self::assertMatchesRegularExpression(
+            '/\/tourney\/\d+/',
+            $this->client->getResponse()->headers->get('location'),
+            'Неправильный URL турнира'
+        );
+        $crawler = $this->client->request('GET', $this->client->getResponse()->headers->get('location'));
+        self::assertResponseIsSuccessful();
+        self::assertCount(2, $crawler->filter('h3'));
+        self::assertCount(2, $crawler->filter('button'));
+    }
+
+    /**
+     * Создание турнира в отдельном методе
+     *
+     * @return Crawler|null
+     */
     private function createRandomTourney(): ?Crawler
     {
         $this->client = static::createClient();
--- app/tests/Tools/ResultsTest.php
+++ app/tests/Tools/ResultsTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Tools;
 
 use App\Tools\Results;
+use JetBrains\PhpStorm\ArrayShape;
 use PHPUnit\Framework\TestCase;
 
 class ResultsTest extends TestCase
@@ -15,7 +16,6 @@ class ResultsTest extends TestCase
      */
     public function testRoundResults($simpleData)
     {
-        exit(var_dump($simpleData));
         $playoffData = [];
         $ret = Results::roundResults($simpleData, 4, $playoffData);
 
@@ -29,88 +29,65 @@ class ResultsTest extends TestCase
         self::assertIsArray($ret[1]);
         self::assertCount(1, $ret[1]);
 
+        foreach ($ret as $rounds) {
+            foreach ($rounds as $round) {
+                self::assertIsInt($round['firstTeam']);
+                self::assertIsInt($round['firstTeamScore']);
+                self::assertIsInt($round['secondTeam']);
+                self::assertIsInt($round['secondTeamScore']);
+                self::assertIsInt($round['winnerId']);
+            }
+        }
+
         self::assertArrayHasKey('winnerId', $ret[1][0]);
         self::assertIsInt($ret[1][0]['winnerId']);
-//        exit(var_dump($ret));
     }
 
-    public function testGetRandScore()
+    public function testGetRandScore(): void
     {
         $score = Results::getRandScore();
         self::assertLessThan(9, $score, 'Снегерированное значение больше ожидаемого');
     }
 
-    public function providerSimpleData()
+    public function providerSimpleData(): array
     {
         return [
-            'first' => [[
+            'first' => [
                 [
-                    'teamId' => 1,
-                    'teamName' => 'Riga FC'
-                ],
-                [
-                    'teamId' => 2,
-                    'teamName' => 'RFS'
-                ],
-                [
-                    'teamId' => 3,
-                    'teamName' => 'Valmiera FC'
-                ],
-                [
-                    'teamId' => 4,
-                    'teamName' => 'BFC Daugavpils'
-                ],
-                [
-                    'teamId' => 5,
-                    'teamName' => 'FK Liepāja'
-                ],
-                [
-                    'teamId' => 6,
-                    'teamName' => 'FK Metta'
-                ],
-                [
-                    'teamId' => 7,
-                    'teamName' => 'FK Spartaks'
-                ],
-                [
-                    'teamId' => 8,
-                    'teamName' => 'FK Ventspils'
-                ],
-            ]],
-//            [
-//                [
-//                    'teamId' => 102,
-//                    'teamName' => 'Riga FC'
-//                ],
-//                [
-//                    'teamId' => '2',
-//                    'teamName' => 'RFS'
-//                ],
-//                [
-//                    'teamId' => 32,
-//                    'teamName' => 'Valmiera FC'
-//                ],
-//                [
-//                    'teamId' => 441,
-//                    'teamName' => 'BFC Daugavpils'
-//                ],
-//                [
-//                    'teamId' => 5111,
-//                    'teamName' => 'FK Liepāja'
-//                ],
-//                [
-//                    'teamId' => 006,
-//                    'teamName' => 'FK Metta'
-//                ],
-//                [
-//                    'teamId' => 'dddd',
-//                    'teamName' => 'FK Spartaks'
-//                ],
-//                [
-//                    'teamId' => 9182,
-//                    'teamName' => 'FK Ventspils'
-//                ],
-//            ],
+                    [
+                        'teamId' => 1,
+                        'teamName' => 'Riga FC'
+                    ],
+                    [
+                        'teamId' => 2,
+                        'teamName' => 'RFS'
+                    ],
+                    [
+                        'teamId' => 3,
+                        'teamName' => 'Valmiera FC'
+                    ],
+                    [
+                        'teamId' => 4,
+                        'teamName' => 'BFC Daugavpils'
+                    ],
+                    [
+                        'teamId' => 5,
+                        'teamName' => 'FK Liepāja'
+                    ],
+                    [
+                        'teamId' => 6,
+                        'teamName' => 'FK Metta'
+                    ],
+                    [
+                        'teamId' => 7,
+                        'teamName' => 'FK Spartaks'
+                    ],
+                    [
+                        'teamId' => 8,
+                        'teamName' => 'FK Ventspils'
+                    ],
+                ]
+            ],
         ];
     }
 }
