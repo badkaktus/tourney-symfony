9f97397cba6ada994f9f4fae076e6f895b83fcac
badkaktus <kolyuchy@gmail.com>
2021-05-12 15:05:50 +0300

    add group test

--- app/tests/Service/GroupsServiceTest.php
+++ app/tests/Service/GroupsServiceTest.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 
+use App\Service\CommandService;
 use App\Service\GroupsService;
+use App\Service\TourneyService;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -11,6 +13,8 @@ class GroupsServiceTest extends KernelTestCase
 {
 
     private GroupsService $groupsService;
+    private CommandService $commandService;
+    private TourneyService $tourneyService;
 
     public function __construct($name = null, array $data = [], $dataName = '')
     {
@@ -20,13 +24,58 @@ class GroupsServiceTest extends KernelTestCase
         $container = self::$container;
         $em = $container->get(\Doctrine\ORM\EntityManagerInterface::class);
         $this->groupsService = new GroupsService($em);
+        $this->commandService = new CommandService($em);
+        $this->tourneyService = new TourneyService($em);
     }
 
-    public function testDrawToGroups()
+    public function testDrawToGroups(): void
     {
+        $tourney = $this->tourneyService->createTourney();
+        $result = $this->groupsService->drawToGroups(
+            $this->commandService->allCommands(),
+            $tourney->getId()
+        );
+
+        self::assertEquals(true, $result);
     }
 
-    public function testCommandsInTourney()
+    public function testCommandsInEmptyTourney(): void
     {
+        $tourney = $this->tourneyService->createTourney();
+        $result = $this->groupsService->commandsInTourney($tourney->getId());
+
+        self::assertCount(2, $result);
+        self::assertArrayHasKey('a', $result);
+        self::assertArrayHasKey('b', $result);
+        self::assertIsArray($result['a']);
+        self::assertCount(0, $result['a']);
+        self::assertIsArray($result['b']);
+        self::assertCount(0, $result['b']);
+    }
+
+    public function testCommandsInNotEmptyTourney(): void
+    {
+        $tourney = $this->tourneyService->createTourney();
+        $this->groupsService->drawToGroups(
+            $this->commandService->allCommands(),
+            $tourney->getId()
+        );
+        $result = $this->groupsService->commandsInTourney($tourney->getId());
+
+        self::assertCount(2, $result);
+        self::assertArrayHasKey('a', $result);
+        self::assertArrayHasKey('b', $result);
+        self::assertIsArray($result['a']);
+        self::assertCount(8, $result['a']);
+        self::assertIsArray($result['b']);
+        self::assertCount(8, $result['b']);
+        foreach ($result['a'] as $key=>$value) {
+            self::assertIsInt($key);
+            self::assertIsInt($value);
+        }
+        foreach ($result['b'] as $key=>$value) {
+            self::assertIsInt($key);
+            self::assertIsInt($value);
+        }
     }
 }
